d7a52472e7e1f8e80fef3b03e7eb023bca5e50d9
deeeki <deeeki@gmail.com>
2022-02-28 00:11:14 +0900

    mount vue component

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,25 @@
 // Entry point for the build script in your package.json
 import "@hotwired/turbo-rails"
 import "./controllers"
+
+import { createApp } from 'vue'
+
+const context = require.context(
+  'components',
+  true,
+  /.+.vue$/
+)
+
+const components = {}
+context.keys().forEach(key => {
+  const name = key.match(/.+\/(.+)\.vue$/)[1]
+  components[name] = context(key).default
+})
+
+window.addEventListener('load', () => {
+  [...document.querySelectorAll('[data-vue]')].forEach(el => {
+    const type = Object.assign(components[el.dataset.vue], { el })
+    const vm = createApp(type)
+    vm.mount(el)
+  })
+})

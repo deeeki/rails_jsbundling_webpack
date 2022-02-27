08b3b9568238c8fc5cfa86f0c0675ef118b8fe0e
deeeki <deeeki@gmail.com>
2022-02-28 00:20:17 +0900

    vue configuration

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path    = require("path")
 const webpack = require("webpack")
+const { VueLoaderPlugin } = require('vue-loader')
 
 module.exports = {
   mode: "production",
@@ -12,9 +13,25 @@ module.exports = {
     sourceMapFilename: "[name].js.map",
     path: path.resolve(__dirname, "app/assets/builds"),
   },
+  resolve: {
+    extensions: ['.js', '.ts', '.vue', '.css', '.scss'],
+    modules: [
+      path.join(__dirname, 'app/javascript'),
+      'node_modules'
+    ],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.vue$/,
+        loader: 'vue-loader'
+      },
+    ]
+  },
   plugins: [
     new webpack.optimize.LimitChunkCountPlugin({
       maxChunks: 1
-    })
+    }),
+    new VueLoaderPlugin()
   ]
 }

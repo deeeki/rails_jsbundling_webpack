48750404aef98f9fba9341468d5653ba50d31e7f
deeeki <deeeki@gmail.com>
2022-03-08 00:47:27 +0900

    typescript

--- app/javascript/components/Sandbox.vue
+++ app/javascript/components/Sandbox.vue
@@ -1,6 +1,16 @@
 <template>
   <h2>
-    Vue
+    {{ title }}
   </h2>
   <img src="../images/rails/rails_welcome.png" width="100" />
 </template>
+
+<script lang="ts">
+export default {
+  data (): { title: string } {
+    return {
+      title: 'Vue'
+    }
+  }
+}
+</script>
